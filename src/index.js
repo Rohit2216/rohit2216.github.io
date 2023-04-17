b3a764ab7a846730d7a97cbbb0b7184573e3fe1a
Rohit2216 <chauhanrohit716@gmail.com>
2023-04-18 00:36:50 +0530

    done

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ const resume_btn2 = document.querySelector("#resume-button-1");
 
 resume_btn.addEventListener("click", (e) => {
   e.preventDefault();
-  window.open(
-    "https://drive.google.com/file/d/13NOplGRKospEeCsnxJYPL8iGWhwKR5h8/view?usp=sharing"
-  );
+  // window.open(
+  //   "https://drive.google.com/uc?export=download&id=13NOplGRKospEeCsnxJYPL8iGWhwKR5h8"
+  // );
   window.location.href =
-    "https://drive.google.com/file/d/13NOplGRKospEeCsnxJYPL8iGWhwKR5h8/view?usp=sharing";
+    "https://drive.google.com/uc?export=download&id=13NOplGRKospEeCsnxJYPL8iGWhwKR5h8";
 });
 
 resume_btn2.addEventListener("click", (e) => {
   e.preventDefault();
-  window.open(
-    "https://drive.google.com/file/d/13NOplGRKospEeCsnxJYPL8iGWhwKR5h8/view?usp=sharing"
-  );
+  // window.open(
+  //   "https://drive.google.com/file/d/13NOplGRKospEeCsnxJYPL8iGWhwKR5h8/view?usp=sharing"
+  // );
   window.location.href =
-    "https://drive.google.com/file/d/13NOplGRKospEeCsnxJYPL8iGWhwKR5h8/view?usp=sharing";
+    "https://drive.google.com/uc?export=download&id=13NOplGRKospEeCsnxJYPL8iGWhwKR5h8";
 });
