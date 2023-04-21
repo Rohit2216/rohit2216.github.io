6a88b00055956e2efd235f3611aebeb1f649fe80
Rohit2216 <chauhanrohit716@gmail.com>
2023-04-22 00:24:47 +0530

    done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const resume_btn2 = document.querySelector("#resume-button-1");
 resume_btn.addEventListener("click", (e) => {
   e.preventDefault();
   // window.open(
-  //   "https://drive.google.com/uc?export=download&id=13NOplGRKospEeCsnxJYPL8iGWhwKR5h8"
+  //   "https://drive.google.com/file/d/13NOplGRKospEeCsnxJYPL8iGWhwKR5h8/view?usp=sharing"
   // );
   window.location.href =
     "https://drive.google.com/uc?export=download&id=13NOplGRKospEeCsnxJYPL8iGWhwKR5h8";
@@ -12,9 +12,9 @@ resume_btn.addEventListener("click", (e) => {
 
 resume_btn2.addEventListener("click", (e) => {
   e.preventDefault();
-  // window.open(
-  //   "https://drive.google.com/file/d/13NOplGRKospEeCsnxJYPL8iGWhwKR5h8/view?usp=sharing"
-  // );
+  window.open(
+    "https://drive.google.com/file/d/13NOplGRKospEeCsnxJYPL8iGWhwKR5h8/view?usp=sharing"
+  );
   window.location.href =
     "https://drive.google.com/uc?export=download&id=13NOplGRKospEeCsnxJYPL8iGWhwKR5h8";
 });
