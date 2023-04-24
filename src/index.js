b52c67f96e7086b5f0b8ccff7900c0d2ee4b377d
Rohit2216 <chauhanrohit716@gmail.com>
2023-04-24 20:54:08 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const resume_btn = document.querySelector("#resume-button-2");
 const resume_btn2 = document.querySelector("#resume-button-1");
 
-resume_btn.addEventListener("click", (e) => {
+resume_btn2.addEventListener("click", (e) => {
   e.preventDefault();
   // window.open(
   //   "https://drive.google.com/file/d/13NOplGRKospEeCsnxJYPL8iGWhwKR5h8/view?usp=sharing"
@@ -10,7 +10,7 @@ resume_btn.addEventListener("click", (e) => {
     "https://drive.google.com/uc?export=download&id=13NOplGRKospEeCsnxJYPL8iGWhwKR5h8";
 });
 
-resume_btn2.addEventListener("click", (e) => {
+resume_btn.addEventListener("click", (e) => {
   e.preventDefault();
   window.open(
     "https://drive.google.com/file/d/13NOplGRKospEeCsnxJYPL8iGWhwKR5h8/view?usp=sharing"
