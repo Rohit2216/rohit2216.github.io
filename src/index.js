95a6f17481cb8eb7256a0ec379b0641f3ae4d12e
Rohit2216 <chauhanrohit716@gmail.com>
2023-04-24 20:58:06 +0530

    done

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const resume_btn2 = document.querySelector("#resume-button-1");
 
 resume_btn2.addEventListener("click", (e) => {
   e.preventDefault();
-  // window.open(
-  //   "https://drive.google.com/file/d/13NOplGRKospEeCsnxJYPL8iGWhwKR5h8/view?usp=sharing"
-  // );
+  window.open(
+    "https://drive.google.com/file/d/13NOplGRKospEeCsnxJYPL8iGWhwKR5h8/view?usp=sharing"
+  );
   window.location.href =
     "https://drive.google.com/uc?export=download&id=13NOplGRKospEeCsnxJYPL8iGWhwKR5h8";
 });
