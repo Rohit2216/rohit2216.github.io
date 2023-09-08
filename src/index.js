be96fd641292d2f6ed34951a389398f436add949
Rohit2216 <chauhanrohit716@gmail.com>
2023-09-09 00:30:29 +0530

    nodemailer done

--- src/index.js
+++ src/index.js
@@ -18,3 +18,64 @@ resume_btn2.addEventListener("click", (e) => {
   window.location.href =
     "https://drive.google.com/uc?export=download&id=13NOplGRKospEeCsnxJYPL8iGWhwKR5h8";
 });
+
+
+
+async function sendEmail(event) {
+  event.preventDefault(); // Prevent the form from submitting
+
+  // Get values from form inputs
+  const email = document.getElementById('user-email').value;
+  const contact = document.getElementById('user-contact').value;
+  const message = document.getElementById('user-message').value;
+
+  // Create an object to store form data
+  const formData = {
+      email: email,
+      contact: contact,
+      message: message
+  };
+
+  // Send a POST request to your server for email sending
+  try {
+      const response = await fetch('https://nodemailer-82ga.onrender.com/send-email', { // Change the URL to match your backend
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formData)
+      });
+
+      if (response.ok) {
+          showModal('Email sent successfully.', 'email-sent'); // Display a modal pop-up when email is sent successfully
+          console.log('Email sent successfully.');
+      } else {
+          showModal('Email sending failed.'); // Display a modal pop-up when email sending fails
+          console.error('Email sending failed.');
+      }
+  } catch (error) {
+      showModal('An error occurred: ' + error.message); // Display a modal pop-up for errors
+      console.error('An error occurred:', error);
+  }
+}
+
+// Show the modal
+function showModal(message, className = '') {
+  const modal = document.getElementById('myModal');
+  const modalMessage = document.getElementById('modal-message');
+  modalMessage.textContent = message;
+  modalMessage.className = className; // Apply the provided CSS class to the message
+  modal.style.display = 'block';
+}
+
+// Close the modal
+function closeModal() {
+  const modal = document.getElementById('myModal');
+  const form = document.querySelector('.contact_form'); // Select the form element
+
+  // Clear the form fields
+  form.reset();
+
+  modal.style.display = 'none';
+}
+
